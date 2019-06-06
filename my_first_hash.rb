0f435b545ff20d815983e5a70ff98a7970c6c052
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 03:51:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale bone corset" => "5"
+  the_manifest = { "whale bone corset" => 5, "porcelain_vases" => 2, "oil paintings" => 3
+end
   
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
